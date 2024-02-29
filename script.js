232fb97ac41bd3b8559c424c3cdeec16c2e92490
brice2k <brice2k1@gmail.com>
2024-02-29 22:07:32 +0200

    Landing page with media query part 1

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu=document.querySelector(".menu-links")
+    const Icon = document.querySelector(".second-nav-icon ");
+    menu.classList.toggle("open")
+    Icon.classList.toggle("open")
+}
